900445e63d5fac2ec00d253933f3a1eb2be96d5a
Artem Osadchyi <traibles@gmail.com>
2023-09-22 03:40:27 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,279 @@
+﻿#region 1.1
+//Console.WriteLine($"{31} {18} {79}");
+#endregion 1.1
+#region 1.2
+//Console.WriteLine(47 + "  " + 52 + "  " + 150);
+#endregion 1.2
+#region 1.3
+//Console.WriteLine(50);
+//Console.WriteLine(10);
+#endregion 1.3
+#region 1.4
+//Console.WriteLine(5);
+//Console.WriteLine(10);
+//Console.WriteLine(21);
+#endregion 1.4
+#region 1.5
+//Console.WriteLine(1);
+//Console.WriteLine(2); 
+#endregion 1.5
+#region 1.6
+//Console.WriteLine(Math.Round(3.1415926, 3));
+#endregion 1.6
+#region 1.7
+//Console.WriteLine(Math.Round(2.71828, 1));
+#endregion 1.7
+#region 1.8
+//Console.WriteLine("Введите число:");
+//string Temporary = Console.ReadLine();
+//Console.WriteLine($"Вы ввели число {Temporary}");
+#endregion 1.8
+#region 1.9
+//Console.WriteLine("Введите число:");
+//string Temporary = Console.ReadLine();
+//Console.WriteLine(Temporary + " – вот какое число Вы ввели");
+#endregion 1.9
+#region 1.10
+//Console.WriteLine("Введите Ваше имя:");
+//string Temporary = Console.ReadLine();
+//Console.WriteLine(Temporary);
+#endregion 1.10
+#region 1.11
+//Console.WriteLine("Введите название футбольной команды:");
+//string Temporary = Console.ReadLine();
+//Console.WriteLine(Temporary + " – это чемпион!");
+#endregion 1.11
+#region 1.12
+//Console.WriteLine("Введите Ваше имя: ");
+//string Temporary = Console.ReadLine();
+//Console.WriteLine("Привет, " + Temporary + "!");
+#endregion 1.12
+#region 1.13
+//Console.WriteLine("Введите целое число: ");
+//int Temporary = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Следующее за числом " + Temporary + " число – " + (Temporary + 1) + ".");
+//Console.WriteLine("Для числа " + Temporary + " предыдущее число – " + (Temporary - 1) + ".");
+#endregion 1.13
+#region 1.14
+//Console.WriteLine("Введите первое число: ");
+//int a = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите второе число: ");
+//int b = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите третье число: ");
+//int c = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine(a + "  "+ b +"  "+ c);
+#endregion 1.14
+#region 1.15
+//Console.WriteLine("Введите первое число: ");
+//int a = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите второе число: ");
+//int b = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите третье число: ");
+//int c = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите четвертое число: ");
+//int d = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine(a + " " + b + " " + c + " " + d);
+#endregion 1.15
+#region 1.16
+//Console.WriteLine("Введите переменную t: ");
+//int t = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите переменную v: ");
+//int v = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите переменную x: ");
+//int x = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите переменную y: ");
+//int y = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("а) 5 10      б)  100 " + t + "       в) " + x + " 25");
+//Console.WriteLine("   7 см \t 1949 " + v + "\t\t" + x + " " + y);
+#endregion 1.16
+#region 1.17
+//Console.WriteLine("Введите переменную a: ");
+//int a = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите переменную b: ");
+//int b = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите переменную x: ");
+//int x = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите переменную y: ");
+//int y = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("а) 2 кг\t\tб) " + a + " 1\t\tв)  " + x + " " + y);
+//Console.WriteLine("   13 17\t   19 " + b + "\t\t    5 " + y);
+#endregion 1.17
+#region 1.17
+//Console.WriteLine("Введите переменную a: ");
+//int a = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите переменную b: ");
+//int b = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите переменную x: ");
+//int x = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите переменную y: ");
+//int y = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("а) 2 кг\t\tб) " + a + " 1\t\tв)  " + x + " " + y);
+//Console.WriteLine("   13 17\t   19 " + b + "\t\t    5 " + y);
+#endregion 1.17
+
+#region 2.1
+//Console.WriteLine("Введите переменную x: ");
+//int x = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите переменную a: ");
+//int a = Convert.ToInt32(Console.ReadLine());
+//int y1 = 17 * x * x - 6 * x + 13;
+//int y2 = 3 * a * a + 5 * a - 21;
+//Console.WriteLine("Значение функции y, при x = " + x + ": " + y1);
+//Console.WriteLine("Значение функции y, при a = " + a + ": " + y2);
+#endregion 2.1
+#region 2.2
+//Console.WriteLine("Введите переменную a: ");
+//int a = Convert.ToInt32(Console.ReadLine());
+//double y = (a * a + 10) / Math.Sqrt(a * a + 1);
+//Console.WriteLine("Значение функции, при a = " + a + ": " + y);
+#endregion 2.2
+#region 2.3
+//Console.WriteLine("Введите переменную a: ");
+//int a = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите переменную x: ");
+//int x = Convert.ToInt32(Console.ReadLine());
+//double y = Math.Sqrt((2 * a + Math.Sin(Math.Abs(3 * a))) / 3.56);
+//Console.WriteLine("Значение функции, при a = " + a + ": " + y);
+//double y1 = Math.Sin((3.2 + Math.Sqrt(1 + x)) / Math.Abs(5 * x));
+//if (x == 0) Console.WriteLine("неопределенность");
+//else Console.WriteLine("Значение функции, при a = " + x + ": " + y1);
+#endregion 2.3
+#region 2.4
+//Console.WriteLine("Введите сторону квадрата: ");
+//int x = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Переметр равен: " + 4 * x);
+#endregion 2.4
+#region 2.5
+//Console.WriteLine("Введите радиус окружности: ");
+//int x = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Диаметр равен: " + 2 * x);
+#endregion 2.5
+#region 2.6
+//Console.WriteLine("Введите высоту от поверхности земли(в км): ");
+//int x = Convert.ToInt32(Console.ReadLine());
+//int R = 6350;
+//double Result = Math.Sqrt((R + x) * (R + x) - R*R);
+//Console.WriteLine("Расстанояние до линии горизонта равно: " + Result);
+#endregion 2.6
+#region 2.7
+//Console.WriteLine("Введите длинну ребра куба: ");
+//int x = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Объем куба равен: " + x * x * x);
+//Console.WriteLine("Объем боковой поверхности: " + x * x);
+#endregion 2.7
+#region 2.8
+//Console.WriteLine("Введите радиус окружности: ");
+//int x = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Длинна окружности: " + (x * 2) * Math.PI);
+//Console.WriteLine("Площадь окружности: " + (x * x) * Math.PI);
+#endregion 2.8
+#region 2.9
+//Console.WriteLine("Введите значение x для функции: ");
+//int x = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите значение y для функции: ");
+//int y = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите значение a для функции: ");
+//int a = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите значение b для функции: ");
+//int b = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Значение функции z = " + (2 * x * x * x - 3.44 * x * y + 2.3 * x * x - 7.1 * y + 2));
+//Console.WriteLine("Значение функции x = " + (3.14 * Math.Pow(a + b, 3) + 2.75 * b * b - 12.7 * a - 4.1));
+#endregion 2.9
+#region 2.10
+//Console.WriteLine("Введите число 1: ");
+//int x = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите число 2: ");
+//int y = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Среднее арифметическое = " + (x + y) /2);
+//Console.WriteLine("Среднее геометрическое = " + Math.Sqrt(x*y));
+#endregion 2.10
+#region 2.11
+//Console.WriteLine("Введите объем тела: ");
+//int x = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите число массу тела: ");
+//int y = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Плотность тела = " + (y / x));
+#endregion 2.11
+#region 2.12
+//Console.WriteLine("Введите количество жителей в государстве: ");
+//int x = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите площадь территорииг государства: ");
+//int y = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Плотность населения = " + (x / y));
+#endregion 2.12
+
+#region 2.13
+//Console.WriteLine("Решаем уравнение: ax + b = 0 при этом (a ≠ 0)");
+//Console.WriteLine("Введите a: ");
+//int a = Convert.ToInt32(Console.ReadLine());
+//while (a == 0) 
+//{
+//    Console.WriteLine("a не может быть равной нулю!");
+//    Console.WriteLine("Введите a: ");
+//    a = Convert.ToInt32(Console.ReadLine());
+//}
+//Console.WriteLine("Введите b: ");
+//int b = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine($"x = {-b / a}");
+#endregion 2.13
+#region 2.14
+//Console.WriteLine("Введите катет а");
+//uint a = Convert.ToUInt32(Console.ReadLine());
+//Console.WriteLine("Введите катет b: ");
+//uint b = Convert.ToUInt32(Console.ReadLine());
+//Console.WriteLine($"Гипотенуза = {Math.Sqrt(Math.Pow(a,2)+ Math.Pow(b, 2))}");
+#endregion 2.14
+#region 2.15
+//Console.WriteLine("Введите радиус внешнего кольца");
+//uint R = Convert.ToUInt32(Console.ReadLine());
+//Console.WriteLine("Введите радиус внутреннего кольца");
+//uint r = Convert.ToUInt32(Console.ReadLine());
+//Console.WriteLine($"Площадь кольца = {Math.PI * Math.Pow(R, 2) - Math.PI * Math.Pow(r, 2)}");
+#endregion 2.15
+#region 2.16
+//Console.WriteLine("Введите катет а");
+//uint a = Convert.ToUInt32(Console.ReadLine());
+//Console.WriteLine("Введите катет b: ");
+//uint b = Convert.ToUInt32(Console.ReadLine());
+//Console.WriteLine($"Периметр = {Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2)) + a + b}");
+#endregion 2.16
+#region 2.17    
+//Console.WriteLine("Введите основание а");
+//uint a = Convert.ToUInt32(Console.ReadLine());
+//Console.WriteLine("Введите основание b");
+//uint b = Convert.ToUInt32(Console.ReadLine());
+//Console.WriteLine("Введите высоту трапеции");
+//uint h = Convert.ToUInt32(Console.ReadLine());
+//Console.WriteLine($"Периметр = {2 * Math.Sqrt(Math.Pow(h,2) + Math.Pow((a-b) / 2, 2)) + a + b}");
+#endregion 2.17
+#region 2.18
+//Console.WriteLine("Введите x");
+//double x = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine("Введите y");
+//double y = Convert.ToDouble(Console.ReadLine());
+//if (x != 0) Console.WriteLine($"Функция z = {(x + (2 + y) / Math.Pow(x, 2)) / (y + 1 / (Math.Sqrt(Math.Pow(x, 2) + 10)))}");
+//else Console.WriteLine("Неопределённость");
+//Console.WriteLine($"Функция q = {7.25 * Math.Sin(x) - Math.Abs(y)}");
+#endregion
+#region 2.19
+//Console.WriteLine("Введите a");
+//double a = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine("Введите b");
+//double b = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine($"Функция x = { (2 / (Math.Pow(a, 2) + 25) + b ) / ( Math.Sqrt(b) + (a + b) / 2) }");
+//if (a != 0) Console.WriteLine($"Функция y = { ( Math.Abs(a) + 2 * Math.Sin(b) ) / ( 5.5 * a)  }");
+//else Console.WriteLine("Неопределённость");
+#endregion 2.19
+
+string[] names = { "Stys", "Lox", "Pidr","Stoker","Fuckin_Slave" };
+
+foreach (string name in names) 
+    if (name.Contains('S')) Console.WriteLine(name);
+
+names = names
+    .Where(x => x.Contains('S'))
+    .Select(x => x.Replace('S', '_'))
+    .ToArray();
+
+foreach (string name in names) 
+    Console.WriteLine(name);
